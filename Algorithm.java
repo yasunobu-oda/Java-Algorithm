bb81013c656b066a8d905399aaf36e8aa2d1cd40
yasunobu-oda <yasunobu.oda.tpi@gmail.com>
2017-10-21 23:14:07 +0900

    【Java勉強用】定番アルゴリズム実装プログラム　微調整

--- Algorithm.java
+++ Algorithm.java
@@ -10,15 +10,15 @@ class runAlg{
 		out.println(clsConst.TITLE + clsConst.NEW_LINE);
 
 		// 処理クラスを設定
-		clsProcess[] clsP = new clsProcess[2];
-		clsP[0] = new clsProcess0();  // アルゴリズム用乱数生成
-		clsP[1] = new clsProcess1();  // 線形探索法（リニアサーチ）
+		clsProcess[] cProcess = new clsProcess[2];
+		cProcess[0] = new clsProcess0();  // アルゴリズム用乱数生成
+		cProcess[1] = new clsProcess1();  // 線形探索法（リニアサーチ）
 		
 		// パラメータチェック
 		boolean bPara =chkPara(args);
 		if (bPara == false){
 			for (int i = 0; i <= clsConst.MAX_PROCESS; i++){
-				out.println(i + "：" + clsP[i].strPro_Name);
+				out.println(i + "：" + cProcess[i].sPro_Name);
 			}
 			return;
 		}
@@ -26,8 +26,8 @@ class runAlg{
 		// 処理呼び出し
 		int iNum = Integer.valueOf(args[0]);
 		if (iNum <= clsConst.MAX_PROCESS){
-			clsP[iNum].args = args;
-			clsP[iNum].subMain();
+			cProcess[iNum].args = args;
+			cProcess[iNum].subMain();
 		} else {
 			out.println("対応する処理がありません。");
 			return;
@@ -69,17 +69,17 @@ abstract class clsProcess implements itfProcess{
 	// パラメータ受取り用
 	String args[];
 	// 処理毎変数初期セット
-	int intParaCnt;
-	String strParaGuide = "ガイド未設定";
-	String strPro_Name = "処理：未実装";
-	String strPro_Desc = "処理概要：未実装";
+	int iParaCnt;
+	String sParaGuide = "ガイド未設定";
+	String sPro_Name = "処理：未実装";
+	String sPro_Desc = "処理概要：未実装";
 	// 定番アルゴリズムで使用する配列数（どの処理でも使うので抽象クラスで設定しておく）
 	ArrayList<Integer> lArray = new ArrayList<Integer>();
 
 	// メイン開始
 	public void subMain(){
 		// タイトル表示
-		out.println(strPro_Name + clsConst.NEW_LINE + strPro_Desc + clsConst.NEW_LINE);
+		out.println(sPro_Name + clsConst.NEW_LINE + sPro_Desc + clsConst.NEW_LINE);
 
 		// パラメータチェック
 		if (chkPara() == false){
@@ -93,6 +93,9 @@ abstract class clsProcess implements itfProcess{
 		// 配列をシャッフルして、ランダムにする
 		Collections.shuffle(lArray);
 		
+		// シャッフルした結果を表示させる
+		System.out.println(lArray);
+
 		// 各アルゴリズムのメイン処理
 		subMainOriginal();
 	}
@@ -105,16 +108,16 @@ abstract class clsProcess implements itfProcess{
 		if (args.length > 1){
 			if (args[1].equals("?")){
 				// パラメータガイドメッセージ表示
-				out.println(strParaGuide);
+				out.println(sParaGuide);
 				bPara = false;
 				return bPara;
 			}
 		}
-		if (args.length != intParaCnt){
+		if (args.length != iParaCnt){
 			out.println("パラメータ数が違います。");
 			out.println("下記パラメータを設定してください。" + clsConst.NEW_LINE);
 			// パラメータガイドメッセージ表示
-			out.println(strParaGuide);
+			out.println(sParaGuide);
 			bPara = false;
 			return bPara;
 		}
@@ -135,11 +138,11 @@ class clsProcess0 extends clsProcess{
 	// コンストラクタ
 	clsProcess0(){
 		// 処理毎変数セット
-		intParaCnt = 2;
-		strParaGuide = "パラメータ１：処理番号（0固定）" + clsConst.NEW_LINE;
-		strParaGuide += "パラメータ２：数値（乱数上限）" + clsConst.NEW_LINE;
-		strPro_Name = "アルゴリズム用乱数生成（乱数を表示させるだけ）";
-		strPro_Desc = "アルゴリズム処理用の乱数を作成する処理（乱数を表示させるだけ）";
+		iParaCnt = 2;
+		sParaGuide = "パラメータ１：処理番号（0固定）" + clsConst.NEW_LINE;
+		sParaGuide += "パラメータ２：数値（乱数上限）" + clsConst.NEW_LINE;
+		sPro_Name = "アルゴリズム用乱数生成（乱数を表示させるだけ）";
+		sPro_Desc = "アルゴリズム処理用の乱数を作成する処理（乱数を表示させるだけ）";
 	}
 	// 個別チェック処理
 	protected boolean chkParaOriginal(){
@@ -152,8 +155,7 @@ class clsProcess0 extends clsProcess{
 	}
 	// メイン処理
 	protected void subMainOriginal(){
-		// シャッフルした結果を表示させる
-		System.out.println(lArray);
+		// 特に処理なし
 	}
 }
 
@@ -164,12 +166,12 @@ class clsProcess1 extends clsProcess{
 	// コンストラクタ
 	clsProcess1(){
 		// 処理毎変数セット
-		intParaCnt = 3;
-		strParaGuide = "パラメータ１：処理番号（0固定）" + clsConst.NEW_LINE;
-		strParaGuide += "パラメータ２：数値（乱数上限）" + clsConst.NEW_LINE;
-		strParaGuide += "パラメータ３：数値（検索数値）" + clsConst.NEW_LINE;
-		strPro_Name = "未実装：線形探索法（リニアサーチ）";
-		strPro_Desc = "未実装：線形探索法のプログラム演習";
+		iParaCnt = 3;
+		sParaGuide = "パラメータ１：処理番号（0固定）" + clsConst.NEW_LINE;
+		sParaGuide += "パラメータ２：数値（乱数上限）" + clsConst.NEW_LINE;
+		sParaGuide += "パラメータ３：数値（検索数値）" + clsConst.NEW_LINE;
+		sPro_Name = "未実装：線形探索法（リニアサーチ）";
+		sPro_Desc = "未実装：線形探索法のプログラム演習";
 	}
 	// 個別チェック処理
 	protected boolean chkParaOriginal(){
@@ -182,7 +184,7 @@ class clsProcess1 extends clsProcess{
 	}
 	// メイン処理
 	protected void subMainOriginal(){
-		// シャッフルした結果を表示させる
+
 		System.out.println("未実装");
 	}
 }
